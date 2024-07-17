dfc210fe8a43c151d1193509b080311670a4dc38
Kudurupaka Yatheesh <yatheeshk@pathbreakertech.com>
2024-07-17 14:25:52 +0530

    java file added

--- javasonarqube.sh
+++ javasonarqube.sh
@@ -31,14 +31,14 @@ sudo mv /opt/sonarqube-10.0.0.68432 /opt/sonarqube
 
 #sudo useradd sonar
 #sudo groupadd sonar
- sudo groupadd sonar1
-#sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar1 sonar1
-sudo useradd -d /opt/sonarqube -g sonar1 sonar1
-sudo chown -R sonar1:sonar1 /opt/sonarqube
+ sudo groupadd sonar
+#sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
+sudo useradd -d /opt/sonarqube -g sonar sonar
+sudo chown -R sonar:sonar /opt/sonarqube
 sudo cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
 sudo sh -c 'cat <<EOF> /opt/sonarqube/conf/sonar.properties
-sonar.jdbc.username=dbsonar
-sonar.jdbc.password=kushal@15
+sonar.jdbc.username=sonar
+sonar.jdbc.password=sonar
 sonar.jdbc.url=jdbc:postgresql://localhost:5432/dbsonarqube
 sonar.web.host=0.0.0.0
  sonar.web.port=9000
@@ -48,8 +48,9 @@ sonar.web.host=0.0.0.0
  sonar.path.logs=logs
 EOF'
 
-#cat /bin/linux-x86-64/sonar.sh
-#RUN_AS_USER=dbsonar
+sudo sh -c cat' <<EOF> /bin/linux-x86-64/sonar.sh
+RUN_AS_USER=sonar
+EOF
 
 #setup systemd service for sonarqube
 sudo sh -c cat' <<EOF> /etc/systemd/system/sonar.service
